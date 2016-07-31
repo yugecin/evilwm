0a216b587866113181cba28f53a73363fd6a4787
yugecin <robin.claerhout@gmail.com>
2016-07-31 15:34:08 +0200

    basic panel gaps

--- evilwm.h
+++ evilwm.h
@@ -229,6 +229,10 @@ extern unsigned int     grabmask1;
 extern unsigned int     grabmask2;
 extern unsigned int     altmask;
 extern char             **opt_term;
+extern int              opt_gap0;
+extern int              opt_gap1;
+extern int              opt_gap2;
+extern int              opt_gap3;
 extern int              opt_bw;
 extern int              opt_snap;
 #ifdef SOLIDDRAG
